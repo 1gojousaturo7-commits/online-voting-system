445bb492f15dca22124c13a14e9af3e07ba11ef6
1gojousaturo7-commits <1.gojou.saturo7@gmail.com>
2026-04-08 15:54:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,44 @@
+import socket
 
+# Use 0.0.0.0 for online platforms like Replit
+HOST = "0.0.0.0"
+PORT = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Server started... Waiting for connections...")
+
+votes = {"A": 0, "B": 0, "C": 0}
+voted_users = set()
+
+while True:
+    client, addr = server.accept()
+    print(f"Connected with {addr}")
+
+    try:
+        # Receive username
+        username = client.recv(1024).decode()
+
+        if username in voted_users:
+            client.send("You have already voted!".encode())
+        else:
+            client.send("Vote for A, B, or C:".encode())
+
+            vote = client.recv(1024).decode().upper()
+
+            if vote in votes:
+                votes[vote] += 1
+                voted_users.add(username)
+                client.send("Vote submitted successfully!".encode())
+            else:
+                client.send("Invalid vote!".encode())
+
+        print("Current Votes:", votes)
+
+    except:
+        print("Error occurred with client")
+
+    finally:
+        client.close()
